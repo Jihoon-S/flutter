23e7402d2e1530e62fe6424d2e6792aef1f09bd1
jihoon <jih.seo23@gmail.com>
2023-09-04 14:07:40 +0900

    modify W2

--- solve/lib/w2.dart
+++ solve/lib/w2.dart
@@ -23,54 +23,52 @@ class W2Widget extends StatefulWidget {
 class _W2WidgetState extends State<W2Widget> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width * 0.5,
-          height: 100,
-          padding: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Colors.blue[200],
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Icon(
-                Icons.add_box_outlined,
-                size: 60,
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '댕댕이',
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.blue[700],
-                      ),
+    return Center(
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.5,
+        height: 100,
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.blue[200],
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.add_box_outlined,
+              size: 60,
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '댕댕이',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.blue[700],
                     ),
-                    const Text(
-                      '댕댕이도 귀여워',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w300,
-                      ),
+                  ),
+                  const Text(
+                    '댕댕이도 귀여워',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w300,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              const Icon(
-                Icons.arrow_forward,
-                size: 30,
-              ),
-            ],
-          ),
+            ),
+            const Icon(
+              Icons.arrow_forward,
+              size: 30,
+            ),
+          ],
         ),
       ),
     );
