9462cf3a421cee3673962c3c5206be01186a68e1
jihoon <jih.seo23@gmail.com>
2023-09-08 15:40:35 +0900

    commit W7

--- solve/lib/header.dart
+++ solve/lib/header.dart
@@ -13,3 +13,4 @@ export 'package:solve_jh/w3.dart' show W3;
 export 'package:solve_jh/w4.dart' show W4;
 export 'package:solve_jh/w5.dart' show W5;
 export 'package:solve_jh/w6.dart' show W6;
+export 'package:solve_jh/w7.dart' show W7;
---
+++ solve/lib/w7.dart
@@ -0,0 +1,81 @@
+library solve_jh;
+
+import 'package:flutter/material.dart';
+import 'package:lib/study_lib.dart';
+
+class W7 extends IWidgetTest {
+  @override
+  String get authorName => 'jh';
+
+  @override
+  StatefulWidget createWidget(BuildContext context, Key key) {
+    return W7Widget(key: key);
+  }
+}
+
+class W7Widget extends StatefulWidget {
+  const W7Widget({super.key});
+
+  @override
+  State<W7Widget> createState() => _W7WidgetState();
+}
+
+class _W7WidgetState extends State<W7Widget> implements ITickerState {
+  double stop = 0.0;
+
+  @override
+  void onTick(Duration elapsed, Duration delta) {
+    stop += 1.0 * (delta.inMilliseconds / 1000);
+
+    if (stop > 0.99) stop = 0.0;
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      shaderCallback: (bounds) {
+        return RadialGradient(
+          // center: Alignment.topLeft,
+          radius: 1.0,
+          colors: const [
+            Colors.red,
+            Colors.yellow,
+          ],
+          stops: [0.0, stop],
+          tileMode: TileMode.decal,
+        ).createShader(bounds);
+      },
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'KEEP CALM',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 50,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            'AND',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 50,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            'DON\'T FEED THE TROLL',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 50,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
